563ead8ad091c6f4931cf13426d3f67ac94b32c3
Jéfferson Santos <109246460+group4mr@users.noreply.github.com>
2022-10-01 14:48:49 +0100

    Improves first class

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { THEME } from './src/styles/theme';
 import Loading from './src/components/Loading';
 import SignIn from './src/screens/SignIn';
 
+import { THEME } from './src/styles/theme';
+
 export default function App() {
+
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+  
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar 
